948d60522449ea8c669ab7ca8e9637a86234b842
杨伟韦 <865562227@qq.com>
2020-10-10 01:39:27 +0800

    录音时间

--- pages/work-judge/index.js
+++ pages/work-judge/index.js
@@ -11,7 +11,7 @@ var startDate;//开始时刻
 var penType = "drawPen";
 var colorStr = "#000";
 var operationType = "mapping";
-var time = 0.5;   // 录音时长（分钟）
+var time = 2;   // 录音时长（分钟）
 var secondes = 0;
 var timer = null;
 let scrollTop = 0;
